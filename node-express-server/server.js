8fdcedcea89e684b649a943554baaccbb68df782
Sumit <sumitthakur16000@gmail.com>
2026-07-09 01:06:20 +0530

    Update server.js

--- node-express-server/server.js
+++ node-express-server/server.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = 8010;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
