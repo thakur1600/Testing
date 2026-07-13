57bedf95b158c0c8d839309ff9b8c7cb82a83757
Sumit <sumitthakur16000@gmail.com>
2026-07-13 23:30:14 +0530

    Update http-common.js

--- react-client/src/http-common.js
+++ react-client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'http://localhost:8010/api',
+  baseURL: 'http://100.25.219.91:8010/api',
   headers: {
     "Content-type": "application/json"
   }
