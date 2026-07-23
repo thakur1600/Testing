df62a6dd8a80ac4ced6c9b1880035ce8cc225946
Sumit <sumitthakur16000@gmail.com>
2026-07-23 11:11:57 +0530

    Update server.js

--- node-express-server/server.js
+++ node-express-server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: process.env.CLIENT_ORIGIN || "http://localhost:8081"
+  origin: process.env.CLIENT_ORIGIN 
 };
 
 
